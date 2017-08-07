4daeb80b7b6dab4d4e4ffb342478bc0eb9e6c4c0
AtreidesEnd <jmeek13@gmail.com>
2017-08-07 20:40:24 +0100

    appmain section wired up and functioning

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 
 import AppNav from '../containers/app-nav';
+import AppMain from '../containers/app-main';
 
 export default class App extends Component {
   render() {
     return (
       <div className="page-content mdl-grid">
         <AppNav />
-        <div className="app-main-cell mdl-cell mdl-cell--10-col">
-          <div className="app-main-div mdl-shadow--4dp">
-            <div className="app-main-core"></div>
-            <div className="app-main-det"></div>
-          </div>
-        </div>
+        <AppMain />
       </div>
     );
   }
---
+++ src/containers/app-main.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class AppMain extends Component {
+
+  renderMain() {
+    // todo replace the below with actual modules for each
+    const appModules = {
+      'Dashboard': {title:"This is the dashboard module.", hasDetail:false},
+      'Forecasts': {title:"This is the forecasts module.", hasDetail:false},
+      'Valuation Model': {title:"This is the valuation module.", hasDetail:true},
+      'Assumptions': {title:"This is the assumptions module.", hasDetail:true},
+      'Change Tracker': {title:"This is the change tracker module.", hasDetail:true},
+    }
+
+    let appDiv;
+    if (this.props.selected && appModules[this.props.selected].hasDetail) {
+      console.log("Module with detail found");
+      appDiv = (
+        <div className="app-main-div mdl-shadow--4dp">
+          <div className="app-main-core">{appModules[this.props.selected].title}</div>
+          <div className="app-main-det">Detail section here</div>
+        </div>
+      );
+    } else if (this.props.selected) {
+      console.log("Module with no detail found");
+      appDiv = (
+        <div className="app-main-div mdl-shadow--4dp">
+          <div className="app-main-core">{appModules[this.props.selected].title}</div>
+        </div>
+      );
+    } else {
+      console.log("no module found");
+      appDiv = <div className="app-main-div mdl-shadow--4dp"> Select a module to get started.</div>
+    }
+    return appDiv;
+  }
+
+  render() {
+    return (
+      <div className="app-main-cell mdl-cell mdl-cell--10-col">
+        {this.renderMain()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    selected: state.navs.selectedNav
+  };
+}
+
+export default connect(mapStateToProps)(AppMain);
