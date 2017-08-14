8acc58cba67c05b438cf4851bd4f10ebb9ad13e3
AtreidesEnd <jmeek13@gmail.com>
2017-08-14 14:42:36 +0100

    updating table header component

--- src/components/valtable-header.js
+++ src/components/valtable-header.js
@@ -7,13 +7,13 @@ export default class ValtableHeader extends Component {
 
   renderTimeCells() {
     let classes = "valmain-table-time-cell";
-    let fixWidth = (100/this.props.timePeriods.length)+'%';
+    let fixWidth = (80/this.props.timePeriods.length)+'%';
     return this.props.timePeriods.map(
       (time) => (<th key={time} className={classes} style={{width:fixWidth}}>{time}</th>));
   }
 
   render() {
-    let headClasses = "valmain-table-header";
+    let headClasses = "valmain-table-head";
     let rowClasses = "valmain-table-head-row";
     let valdriverClasses = "valmain-thead-valdriver";
     return (
